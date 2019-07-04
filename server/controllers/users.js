df34c6ce4d1e7681eb13e2ed700e6f1b3e083595
User-1 <user-1@MacBook-28.local>
2019-07-03 22:20:20 -0600

    getting skeleton setup

---
+++ server/controllers/users.js
@@ -0,0 +1,6 @@
+var app = require('./index.js');
+
+module.exports = {
+  
+  
+};
--- server/index.js
+++ server/index.js
@@ -6,18 +6,15 @@ const config = require('./config.js');
 
 app.use(bodyParser.json());
 
-// massive(config.connection)
-// .then( db => {
-//   app.set('db', db);
-// })
+massive(config.dbConnection)
+.then( db => app.set('db', db))
 
 app.use(express.static(__dirname + './../build'))
 
-const userController = require("./userController.js");
-
-//////////Endpoints for the front end
-
-
+//
+//Endpoints
+//
 
 
-app.listen(config.port, console.log("you are now connected on " + config.port));
+
+app.listen(config.serverPort, console.log("you are now connected on " + config.serverPort));
