9f897b20e8124ec728b56ed63cf4e941975bd0a1
loren pabst <lorenpabst@gmail.com>
2019-07-05 00:29:04 -0600

    got db tests working

--- server/database/users.js
+++ server/database/users.js
@@ -1,14 +1,7 @@
 const { getDb } = require('./massive');
 
-// get the db ready to use in this file
-// this runs once when the server starts up and this file loads
-let db;
-async function initDb() {
-    db = await getDb();
-}
-initDb();
-
 async function createUser(username, passwordHash) {
+    const db = await getDb();
     return db.users.insert({ username, password: passwordHash })
 }
 
--- server/util/test/helpers.test.js
+++ server/util/test/helpers.test.js
@@ -4,9 +4,7 @@ describe("Helpers tests", () => {
     test("hashes password correctly", () => {
         const password = 'myPassword';
         const hash = helpers.hashPassword(password);
-        console.log('received resultf rom hash fnction')
-        console.log(hash);
-        expect(hash).toEqual('this should fail')
+        //TODO: build out helper function then build out this test
     })
 })
 
