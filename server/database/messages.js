03a830ac45ccf23cc0beeee199b3dc278591c959
loren pabst <lorenpabst@gmail.com>
2019-07-05 00:03:11 -0600

    got db accessible from any file

---
+++ server/database/messages.js
@@ -0,0 +1,9 @@
+const { getDb } = require('./massive');
+
+// get the db ready to use in this file
+// this runs once when the server starts up and this file loads
+let db;
+async function initDb() {
+    db = await getDb();
+}
+initDb();
--- server/database/users.js
+++ server/database/users.js
@@ -1,6 +1,15 @@
+const { getDb } = require('./massive');
 
-function createUser(db, username, passwordHash){
-    return db.users.insert({username, password: passwordHash})
+// get the db ready to use in this file
+// this runs once when the server starts up and this file loads
+let db;
+async function initDb() {
+    db = await getDb();
+}
+initDb();
+
+async function createUser(username, passwordHash) {
+    return db.users.insert({ username, password: passwordHash })
 }
 
 module.exports = {
